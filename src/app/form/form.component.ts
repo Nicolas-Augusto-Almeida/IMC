1991b773ad9b8181e7324b198975c2fdb26f82af
Nicolas <nick.nogueira02@gmail.com>
2025-03-25 08:30:03 -0300

    done

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import e from 'express';
 
 @Component({
   selector: 'app-form',
@@ -10,9 +11,37 @@ export class FormComponent {
   altura: number = 0;
   peso: number = 0;
   imc: number = 0;
+  text: string = " ";
+  text2: string = " ";
+  image: string = " ";
+
 
   onClick(){
     this.imc = this.peso / (this.altura * this.altura)
-
+    this.text = 'IMC: '
+    if (this.imc < 18.5){
+      this.text2 = ' Abaixo do peso'
+      this.image = 'IMC.png'
+    }
+    if (this.imc >= 18.5 && this.imc <= 24.9 ){
+      this.text2 = ' Peso normal'
+      this.image = 'Normal.png'
+    }
+    if (this.imc >= 25 && this.imc <= 29.9 ){
+      this.text2 = ' Sobrepeso'
+      this.image = 'Sobrepeso.png'
+    }
+    if (this.imc >= 30 && this.imc <= 34.9 ){
+      this.text2 = ' Obesidade I'
+      this.image = 'Obesidade_I.png'
+    }
+    if (this.imc >= 35 && this.imc <= 39.9 ){
+      this.text2 = ' Obesidade II'
+      this.image = 'Obesidade_II.png'
+    }
+    if (this.imc >= 40 ){
+      this.text2 = ' Obesidade III'
+      this.image = 'Obesidade_III.png'
+    }
   }
 }
